f089c1dd67cbdcf5e5bbd369a18dde06d90ce681
unknowN <71097371+isacandikkaya@users.noreply.github.com>
2022-08-26 23:17:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const Discord = require("discord.js")
 const client = new Discord.Client();       
 const settings = require("./settings.js")    
 const fs = require("fs");                
-require('./util/Loader.js')(client);     // zekiarda.xyz
-client.commands = new Discord.Collection(); // zekiarda.xyz
+require('./util/Loader.js')(client);   
+client.commands = new Discord.Collection(); 
 client.aliases = new Discord.Collection();  
 
-// zekiarda.xyz
+
 
 fs.readdir('./commands/', (err, files) => { 
   if (err) console.error(err);               
-  console.log(`${files.length} komut yüklenecek.`); // zekiarda.xyz
+  console.log(`${files.length} komut yüklenecek.`);
   files.forEach(f => {                       
     let props = require(`./commands/${f}`);   
     console.log(`${props.settings.name} komutu yüklendi.`);  
     client.commands.set(props.settings.name, props); 
-    props.settings.aliases.forEach(alias => {          // zekiarda.xyz
+    props.settings.aliases.forEach(alias => {        
       client.aliases.set(alias, props.settings.name);  
     });
   });
